ea8f177e52cc23fe82f7ac008f5b734b99c98ce9
Stephen Maitland <s.maitland@me.com>
2019-10-31 18:15:15 +0000

    Test Suite

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+const app = express();
+const port = process.env.PORT;
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Service temporarily unavailable');
+// });
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT;
 
 // app.use((req, res, next) => {
 //     res.status(503).send('Service temporarily unavailable');
 // });
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
-
 });
